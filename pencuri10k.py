e2868740afaee38f349b771d57f0d5842f541789
Frizil <160263297+Frizil@users.noreply.github.com>
2024-03-21 16:49:50 +0700

    Add files via upload

---
+++ pencuri10k.py
@@ -0,0 +1,118 @@
+import asyncio
+import sys
+import re
+import time
+from telethon import TelegramClient, events
+
+api_id = 18850178
+api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
+sesi_fil = input('Siapa: ')
+
+cabang = "/mg24_game_Mencuri_44"
+jenis = "_curiUang"
+hapus = "/mg2024_hapusBuronan"
+rumah = "/mg2024_RumahWarga"
+i = 0
+bot = 'catlovy'
+alamat = []  # Menggunakan list untuk menyimpan alamat
+
+async def main():
+    async with TelegramClient(sesi_fil, api_id, api_hash) as clien:
+        await clien.send_message(bot, cabang)
+        
+        @clien.on(events.NewMessage(from_users=bot))
+        async def handler(event):
+            global i, alamat
+            pesan = event.raw_text
+            
+            if "bisa saja polisi menangkap dan memenjarakanmu" in pesan:
+                alamat.clear()  # Menghapus semua elemen dari list
+                i = 0
+                print('=' * 30)
+                print('Kondisi Muncul alamat')
+                print('=' * 30)
+                baris = pesan.split('\n')
+                for baris_teks in baris:
+                    if '/mg2024_x_' in baris_teks:
+                        alamat.append(baris_teks.strip())  # Menambahkan ke list
+                print(alamat)  # Mencetak list alamat
+                await asyncio.sleep(1.5)
+                if alamat:
+                    await event.respond((alamat[i].replace('🏘', '')) + str(jenis))
+                return
+
+            if "Berhasil mencuri uang" in pesan:
+                print('=' * 30)
+                print('Kondisi Berhasil Mencuri Uang')
+                print('=' * 30)
+                i += 1
+                if 1 <= i <= 2:
+                    await asyncio.sleep(0.5)
+                    await event.respond(hapus)
+                elif 3 <= i <= 5:
+                    await asyncio.sleep(0.5)
+                    await event.respond(hapus)
+                return
+              
+            if "Apa kamu yakin ingin menghapus" in pesan:
+                print('=' * 30)
+                print('Konfirmasi Penghapusan Buron')
+                await asyncio.sleep(0.5)
+                await event.click(text="Confirm")
+                return
+            
+            if "Harga buronan dihapus" in pesan or "Saat ini kamu bersih" in pesan:
+                print('=' * 30)
+                print('Kondisi Buron di hapus / Tidak ada buron')
+                print('=' * 30)
+                if i >= 5:
+                    await asyncio.sleep(0.5)
+                    await event.respond(rumah)
+                elif alamat and i < len(alamat):
+                    await event.respond((alamat[i].replace('🏘', '')) + str(jenis))
+                return
+              
+            if "Kumpulkan poin sebanyak-banyaknya" in pesan or "Permainan dimulai" in pesan:
+                print('=' * 30)
+                print('Kumpulkan poin sebanyak-banyaknya / Permainan dimulai')
+                print('=' * 30)
+                await asyncio.sleep(0.5)
+                await event.respond(rumah)
+                return
+              
+            if "Polisi menemukanmu" in pesan:
+                print('=' * 30)
+                print('Kondisi Di Tangkap Polisi')
+                print('=' * 30)
+                
+                # Membagi pesan berdasarkan spasi
+                words = pesan.split()
+                
+                # Mencari posisi kata "menit" dalam daftar kata
+                index = words.index('menit')
+                
+                # Mengambil nilai durasi penjara sebelum kata "menit"
+                captured_value = words[index - 1]
+                
+                if captured_value:  # Memastikan nilai yang diekstrak tidak kosong
+                    durasi_penjara = int(captured_value)
+                    print("Durasi penjara:", durasi_penjara, "menit")
+                    
+                    # Mengkonversi durasi penjara ke dalam detik
+                    detik = durasi_penjara * 60
+                    print("Durasi penjara dalam detik:", detik)
+                    
+                    await asyncio.sleep(detik)
+                    
+                    if alamat and i < len(alamat):
+                        print('=' * 30)
+                        print('Lanjut Mencuri')
+                        print('=' * 30)
+                        await event.respond((alamat[i].replace('🏘', '')) + str(jenis))
+                return
+                        
+        print(time.asctime(), '-', 'Mulai')
+        await clien.run_until_disconnected()
+        print(time.asctime(), '-', 'Berhenti')
+
+asyncio.run(main())

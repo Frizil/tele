4ebe723dbac1ebfeeadd4b3553778df934b203ae
Frizil <160263297+Frizil@users.noreply.github.com>
2024-03-21 21:40:01 +0700

    Update pencuri10k.py

--- pencuri10k.py
+++ pencuri10k.py
@@ -8,7 +8,7 @@ api_id = 18850178
 api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
 sesi_fil = input('Siapa: ')
 
-cabang = "/mg24_game_Mencuri_44"
+cabang = "/mg24_game_Mencuri_40"
 jenis = "_curiuang"
 hapus = "/mg2024_hapusBuronan"
 rumah = "/mg2024_RumahWarga"

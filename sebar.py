2645db3115140ef7b366c9f5ee65e8d64671e3c8
Frizil <160263297+Frizil@users.noreply.github.com>
2024-04-08 08:06:07 +0700

    Update sebar.py

--- sebar.py
+++ sebar.py
@@ -24,7 +24,7 @@ async def main():
         
         # Replace 'destination_group_id' and 'source_group_id' with the actual group IDs
         destination_group_id = -1002067531778  # Example destination group ID
-        source_group_id = -1001332967453  # Example source group ID
+        source_group_id = -1001312846378  # Example source group ID
         
         destination_entity = await client.get_input_entity(destination_group_id)
         source_entity = await client.get_input_entity(source_group_id)

f2092f940df02cf078117fcf6d282cc41a9093c1
Frizil <160263297+Frizil@users.noreply.github.com>
2024-04-10 15:15:03 +0700

    Update homesx.py

--- homesx.py
+++ homesx.py
@@ -11,9 +11,11 @@ sesi_fil = input('Akun : ')
 
 bot = ['danaudalamhutan', 'KampungMaifamXBot', 'KampungMaifamX4Bot', 'KampungMaifamBot']
 result = "/casino_hasil"
-mode = "/casino_FiftyFifty_"
-judi = mode+str(random.randint(1,2))+"_1e12"
-hapus = '/eat_holysnack'
+mode = "/casino_FortuneDice_"
+#mode = "/casino_FiftyFifty_"
+#judi = mode+str(random.randint(1,2))+"_1e12"
+judi = mode+str(random.randint(1,2))+"_5e10"
+hapus = 'Hapus menggunakan Uang'
 total = 0
 
 
